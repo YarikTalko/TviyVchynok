46eeccb9386a4c22683593b217a1d5c5aad50332
arikt <aritalko@gmail.com>
2025-03-01 01:34:41 +0200

    add close button

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -2,11 +2,16 @@ function dropdownMenu() {
     document.getElementById("header-dropdown-container-id").classList.toggle("showMenu");
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target.closest("hamburger-menu-button")) {
         let dropdownMenu = document.getElementById("header-dropdown-container-id");
         if (dropdownMenu.classList.contains("showMenu")) {
             dropdownMenu.classList.remove("showMenu");
         }
+    }
+    if (document.getElementById("hamburger-menu-button-id").getAttribute("src") === "images/hamburger_menu.svg") {
+        document.getElementById("hamburger-menu-button-id").src = "images/close_button.svg";
+    } else {
+        document.getElementById("hamburger-menu-button-id").src = "images/hamburger_menu.svg";
     }
 };
