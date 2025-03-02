662e4332ef9ea6d646820cd12ce9f5b3990b7aba
arikt <aritalko@gmail.com>
2025-03-02 13:21:09 +0200

    change carousel to swiper

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,6 +2,9 @@ const buttonsWrapper = document.querySelector('.carousel-map');
 const slides = document.querySelector('.carousel');
 
 buttonsWrapper.addEventListener('click', (e) => {
+    let width = 250;
+    let gap = 13;
+    let sum = width + gap;
     if (e.target.nodeName === "BUTTON") {
         Array.from(buttonsWrapper.children).forEach(item =>
             item.classList.remove('active')
@@ -10,10 +13,39 @@ buttonsWrapper.addEventListener('click', (e) => {
             slides.style.transform = "translateX(-0%)";
             e.target.classList.add('active');
         } else if (e.target.classList.contains('second')) {
-            slides.style.transform = "translateX(-46.36%)";
+            slides.style.transform = "translateX(-" + sum + "px)";
             e.target.classList.add('active');
         } else if (e.target.classList.contains('third')) {
-            slides.style.transform = "translateX(-54.1%)";
+            sum = sum * 2;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('fourth')) {
+            sum = sum * 3;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('fifth')) {
+            sum = sum * 4;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('sixth')) {
+            sum = sum * 5;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('seventh')) {
+            sum = sum * 6;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('eight')) {
+            sum = sum * 7;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('ninth')) {
+            sum = sum * 8;
+            slides.style.transform = "translateX(-" + sum + "px)";
+            e.target.classList.add('active');
+        } else if (e.target.classList.contains('tenth')) {
+            sum = sum * 8.7;
+            slides.style.transform = "translateX(-" + sum + "px)";
             e.target.classList.add('active');
         }
     }
